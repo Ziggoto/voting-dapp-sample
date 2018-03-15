cc85261e7f1f5d39d7dac82db753a0722db70719
Fábio Theophilo <fabiotheophilo@gmail.com>
2018-03-14 23:56:59 -0300

    Inits template with Bulma

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Eleições 2018</h1>
+          <h2 class="subtitle">
+            A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+				<div class="columns">
+					<div class="column">
+            <div class="notification is-info">First column</div>
+					</div>
+					<div class="column">
+            <div class="notification is-success">Second column</div>
+					</div>
+					<div class="column">
+            <div class="notification is-warning">Third column</div>
+					</div>
+					<div class="column">
+            <div class="notification is-danger">Fourth column</div>
+					</div>
+				</div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -23,30 +35,11 @@
 export default {
   name: 'hello',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js PWA'
-    }
+    return {}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
